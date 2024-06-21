4b5974628c42a497bd74d87924198b9437dfccae
kwon <kckc0608@naver.com>
2024-06-21 16:02:38 +0900

    chore: repository query 에서 멀티라인을 사용하도록 수정

--- src/main/java/ceos/vote/vote/domain/repository/VoteRepository.java
+++ src/main/java/ceos/vote/vote/domain/repository/VoteRepository.java
@@ -13,32 +13,34 @@ import java.util.List;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-    @Query(value =
-            "select new ceos.vote.vote.application.dto.response.DemodayVoteResponse(v.teamName, count(*)) "
-            + "from DemoDayVote v "
-            + "group by v.teamName "
-            + "order by count(*) desc"
-    )
+    @Query(value = """
+            select new ceos.vote.vote.application.dto.response.DemodayVoteResponse(v.teamName, count(*))
+              from DemoDayVote v
+             group by v.teamName
+             order by count(*) desc
+            """)
     List<DemodayVoteResponse> getDemodayVoteCount();
 
-    @Query(value =
-            "select new ceos.vote.vote.application.dto.response.PartLeaderVoteResponse(v.partLeader.name, count(*)) "
-            + "from PartLeaderVote v "
-            + "where v.partLeader.part = :part "
-            + "group by v.partLeader "
-            + "order by count(*) desc"
-    )
+    @Query(value = """
+            select new ceos.vote.vote.application.dto.response.PartLeaderVoteResponse(v.partLeader.name, count(*))
+              from PartLeaderVote v 
+             where v.partLeader.part = :part 
+             group by v.partLeader 
+             order by count(*) desc
+            """)
     List<PartLeaderVoteResponse> getPartLeaderVoteCount(@Param("part") Part part);
 
-    @Query(value =
-            "select count(1) > 0 "
-            + "from DemoDayVote v "
-            + "where v.voteUser.username = :username")
+    @Query(value = """
+            select count(1) > 0 
+              from DemoDayVote v 
+             where v.voteUser.username = :username
+            """)
     boolean existsDemodayVotesByUsername(@Param("username") String username);
 
-    @Query(value =
-            "select count(1) > 0 "
-            + "from PartLeaderVote v "
-            + "where v.voteUser.username = :username ")
+    @Query(value = """
+            select count(1) > 0
+            from PartLeaderVote v
+            where v.voteUser.username = :username
+            """)
     boolean existsPartLeaderVotesByUsername(@Param("username") String username);
 }
