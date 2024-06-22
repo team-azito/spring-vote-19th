899e53cf2cc31b3cdc957448a54d04ce74dad86a
kckc0608 <kckc0608@gmail.com>
2024-06-22 22:40:32 +0900

    refactor: vote fixture 에 있는 User 데이터를 User Fixture로 이동

--- src/test/java/ceos/vote/vote/fixture/VoteFixture.java
+++ src/test/java/ceos/vote/vote/fixture/VoteFixture.java
@@ -1,52 +1,15 @@
 package ceos.vote.vote.fixture;
 
-import ceos.vote.user.domain.Part;
+import static ceos.vote.user.fixture.UserFixture.BACKEND_PART_LEADER;
+import static ceos.vote.user.fixture.UserFixture.FRONTEND_PART_LEADER;
+import static ceos.vote.user.fixture.UserFixture.USER_1;
+import static ceos.vote.user.fixture.UserFixture.USER_2;
+
 import ceos.vote.user.domain.TeamName;
-import ceos.vote.user.domain.User;
 import ceos.vote.vote.domain.DemoDayVote;
 import ceos.vote.vote.domain.PartLeaderVote;
 
 public class VoteFixture {
-
-    private static final User USER_1 = User.builder()
-            .id(1L)
-            .username("hello")
-            .password("hotekvkx124!")
-            .email("test@email.com")
-            .name("이도현")
-            .part(Part.BACK_END)
-            .teamName(TeamName.AZITO)
-            .build();
-
-    private static final User USER_2 = User.builder()
-            .id(2L)
-            .username("hello2")
-            .password("hotekvkx124!")
-            .email("test2@email.com")
-            .name("조유담")
-            .part(Part.FRONT_END)
-            .teamName(TeamName.AZITO)
-            .build();
-
-    private static final User BACKEND_PART_LEADER = User.builder()
-            .id(3L)
-            .username("hello3")
-            .password("hotekvkx124!")
-            .email("test3@email.com")
-            .name("이도현2")
-            .part(Part.BACK_END)
-            .teamName(TeamName.AZITO)
-            .build();
-
-    private static final User FRONTEND_PART_LEADER = User.builder()
-            .id(4L)
-            .username("hello4")
-            .password("hotekvkx124!")
-            .email("test4@email.com")
-            .name("조유담2")
-            .part(Part.FRONT_END)
-            .teamName(TeamName.AZITO)
-            .build();
 
     public static final DemoDayVote DEMO_DAY_VOTE_1 = DemoDayVote.builder()
             .id(1L)
